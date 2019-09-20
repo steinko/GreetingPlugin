7ddb156610ef15560f9894c8ba7571573e27a5b7
steinkorsveien <steinkorsveien@10.0.0.83>
2019-09-20 13:02:27 +0200

    Adde kotlin lint in root build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,17 @@
  
                id("org.jetbrains.kotlin.jvm") version "1.3.50"
                id("org.steinko.greeting")
+               id ("org.jmailen.kotlinter") version "2.0.0"
            }
            
  repositories {
-  jcenter()
+   jcenter()
+   mavenCentral()
+   maven {
+             url =  uri( "https://maven.google.com/")
+         }
+         
+  dependencies  {      
+          implementation ("com.pinterest:ktlint:0.34.2")
+         }
 }
